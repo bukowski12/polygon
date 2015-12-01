1fd056e6ac0c08b363c7ebfdc4da06991a28acac
Vlastimil Wagner <bukowski@miki.hzsoul.cz>
2015-12-01 11:37:32 +0100

    camera matrix thread

--- app/camera_backup3.py
+++ app/camera_backup3.py
@@ -54,35 +54,6 @@ class Ui_Kamery(object):
 
 
 class Grab(QtCore.QThread):
-	def __init__(self, idcam, url, user=None, password=None, parent = None):
-		QtCore.QThread.__init__(self, parent)
-		self.idcam = idcam
-		self.url = url
-		self.user = user
-		self.password = password
-		self.start()
-
-	def run(self):
-		self.openDevice()
-
-	def openDevice(self):
-		self.device = cv2.VideoCapture()
-		try:
-			ret = self.device.open(self.url)
-			frame = self.get_frame()
-		except Exception,e:
-			print str(e)
-			frame = np.full((288,352,3),(0,0,153),dtype=np.uint8)
-			cv2.putText(frame, 'NO SIGNAL', (0,10), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,255))
-		self.emit (QtCore.SIGNAL("output(PyQt_PyObject, PyQt_PyObject)"), self.idcam, frame)
-
-	def get_frame(self):
-		ret,frame = self.device.read()
-		if ret==False:
-			frame = np.full((288,352,3),(0,0,153),dtype=np.uint8)
-		return frame
-
-class CamMatrix(QtCore.QThread):
 	def __init__(self, urls, user=None, password=None, parent = None):
 		QtCore.QThread.__init__(self, parent)
 		self.urls = urls
@@ -91,49 +62,49 @@ class CamMatrix(QtCore.QThread):
 		self.start()
 	
 	def run(self):
-		self.camLoop()
+		self.openDevice()
 
-	def camLoop(self):
-		self.countCam = enumerate(self.urls)
-		self.img = self.createArray()
+	def get_frame(self):
+		#try:
+        #urllib2.urlopen(request)
+#>>> except urllib2.URLError, e:
+ #       print e.reason
+		#response = urllib2.urlopen(self.req)
+		#img_array = np.asarray(bytearray(response.read()), dtype=np.uint8)
+		#frame = cv2.imdecode(img_array, 1)
+		ret,frame = self.device.read()
+		#if self.device.grab():
+		#	frame = self.device.retrieve()
+		return frame
+		#height, width, bytes_per_component = frame.shape
+		#bytes_per_line = bytes_per_component * width
+		#image = QImage(frame.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+		#self.emit(QtCore.SIGNAL("output(QImage)"), image)
+
+	def openDevice(self):
+		self.device = cv2.VideoCapture()
 		while (True):
-			self.graber = [None]*len(self.urls)
+			img = []
 			for i,url in enumerate(self.urls):
-				self.graber[i] = Grab(i, url, self.user, self.password)
-				self.connect (self.graber[i],QtCore.SIGNAL("output(PyQt_PyObject, PyQt_PyObject)"), self.insertImage)
-			for i,url in enumerate(self.urls):
-				self.graber[i].wait(1000)
-			camGrid = self.combineImages(self.img, i+1)
+				try:
+					self.device.open(url)
+					#print self.came.isOpenned()
+					#auth_encoded = base64.encodestring('%s:%s' % (self.user, self.password))[:-1]
+					#self.req = urllib2.Request(url)
+					#self.req.add_header('Authorization', 'Basic %s' % auth_encoded)
+				except Exception,e: print str(e)
+				a = self.get_frame()
+				print "mam obr", i
+				img.append(np.array(a))
+				#img = self.get_frame()
+			camGrid = self.combineImages(img, i)
+			print "mam cely"
 			camGrid = np.asarray(camGrid[:,:])
 			#cv2.imshow('cam',camGrid)
 			height, width, bytes_per_component = camGrid.shape
 			bytes_per_line = bytes_per_component * width
 			image = QImage(camGrid.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
 			self.emit(QtCore.SIGNAL("output(QImage)"), image)
-
-	def insertImage (self, idcam, frame):
-		#cv2.putText(frame, 'Kamera: %d .' % idcam, (0,10), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,255))
-		#del self.img[idcam]
-		#self.img.insert(idcam, np.array(frame))
-		self.img[idcam] = np.array(frame)
-
-		#camGrid = self.combineImages(img, i)
-		#print "mam cely"
-		#camGrid = np.asarray(camGrid[:,:])
-		#cv2.imshow('cam',camGrid)
-		#height, width, bytes_per_component = camGrid.shape
-		#bytes_per_line = bytes_per_component * width
-		#image = QImage(camGrid.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
-		#self.emit(QtCore.SIGNAL("output(QImage)"), image)
-
-	def createArray(self):
-		w,h=352,288
-		img = np.full((288,352,3),(0,0,153),dtype=np.uint8)
-		imgArray = []
-		for i in self.countCam:
-			imgArray.append(img)
-		return (imgArray)
-
 
 
 	#def combineImages(*args):
@@ -158,7 +129,7 @@ class CamMatrix(QtCore.QThread):
 		# all fit.
 		grid = int(math.ceil(math.sqrt(numImages)))
 		combinedImage = cv2.cv.CreateImage((colWidth*grid, rowHeight*grid), 8, 3)
-		cv2.cv.Set(combinedImage, cv2.cv.CV_RGB(0,0,153));
+		cv2.cv.Set(combinedImage, cv2.cv.CV_RGB(50,50,50));
 		for index, img in enumerate(imageArray):
 			# Ensure all images are same type
 			#if img.nChannels == 1:
@@ -170,8 +141,9 @@ class CamMatrix(QtCore.QThread):
 			row = int(math.ceil(index / grid))
 			column = index % grid
 			height, width, channels  = img.shape
-			#cv2.putText(img, "TESThg", (0,10), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,255))
+			cv2.putText(img, "TESThg", (0,10), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,255))
 			img2 = cv2.cv.fromarray(img)
+			print column,row ,width, height
 			cv2.cv.SetImageROI(combinedImage, (column*colWidth, row*rowHeight, width, height))
 			cv2.cv.Copy(img2, combinedImage)
 			cv2.cv.ResetImageROI(combinedImage)
@@ -183,13 +155,41 @@ class Camera(QtGui.QWidget, Ui_Kamery):
 	def __init__(self, urls, user=None, password=None, parent = None):
 		super(Camera, self).__init__(parent)
 		self.setupUi(self)
-		self.worker = CamMatrix(urls, user, password)
+		#QtCore.QThread.__init__(self)
+		self.worker = Grab(urls, user, password)
+		#self.setWindowFlags(QtCore.Qt.Window)
+		#self.setupUi(self)
+		#self.camWidget = QtGui.QWidget(parent)
+		#ui = Ui_Kamery()
+		#ui.setupUi(self.camWidget)
+		#self.camWidget = Ui_Kamery()
+		#camWidget.setWindowFlags(QtCore.Qt.Window)
+		#camWidget.show()
 		self.show()
 		self.is_recording = False
+		#self.camThread = QtCore.QThread()
+		#self.camThread.start()
+		#self.timer = QtCore.QTimer(None)
+		#print self.timer.thread()
+		#self.timer.moveToThread(self.camThread)
+		#print self.timer.thread()
+		#self.timer.setInterval(2000)
+		#self.timer.timeout.connect(self.display)
+		#self.camThread.start()
+		#self.start()
+		#self.openDevice()
 		self.connect(self.worker, QtCore.SIGNAL("output(QImage)"), self.display)
     
 	def display(self, image):
 		self.camWindow.setPixmap(QPixmap.fromImage(image))
+			#	if self.is_recording:
+			#		self.video.write(frame)
+		#except:
+		#	self.camWindow.setText("<span style='font-size:64pt; font-weight:700; color:#ffffff;'>NO SIGNAL</span>")
+		#	self.camWindow.setStyleSheet("background-color: blue")
+		#	self.camWindow.setAlignment(QtCore.Qt.AlignCenter)
+		#	print "chyba display"
+
     
 	def startRecord(self):
 		filename = datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.avi'
